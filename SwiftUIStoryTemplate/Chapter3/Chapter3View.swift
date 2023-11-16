8f183a166a38d2994ab660ddf76af0bf8e348241
Embee <mabintybangoura214@gmail.com>
2023-11-16 11:40:28 -0500

    patient files

--- SwiftUIStoryTemplate/Chapter3/Chapter3View.swift
+++ SwiftUIStoryTemplate/Chapter3/Chapter3View.swift
@@ -2,7 +2,20 @@ import SwiftUI
 
 struct Chapter3View: View {
     var body: some View {
-        Text("Malaya")
+        ZStack{
+            Image("whitepaper")
+            VStack{
+                Image("Patient Files")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 400, height: 100)
+                ScrollView(.horizontal){
+                    HStack(spacing: 2){
+                        Image("clipboard")
+                    }
+                }
+            }
+        }
     }
 }
 
